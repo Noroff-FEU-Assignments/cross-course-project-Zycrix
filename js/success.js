77714e41dc747ee955193944a21391bc8b417ca9
Zycrix <Doffa98@live.no>
2022-04-23 17:54:16 +0200

    Update success.js

--- js/success.js
+++ js/success.js
@@ -4,18 +4,20 @@ import{bestSellers as bestSellers} from "./highlight.js";
 import{apiCall as apiCall} from "./apiCall.js";
 const jackets = await apiCall();
 
-try{
-  
-  //Filter the jackets array and store all jackets with an attribute of "highlight" set to "true"
-  let newJackets = jackets.filter(function (jackets){
-    return jackets.attributes[0].options[0] === "true";
-  });
+if(window.location.href === window.location.origin + "/success.html"){
+  try{
+    
+    //Filter the jackets array and store all jackets with an attribute of "highlight" set to "true"
+    let newJackets = jackets.filter(function (jackets){
+      return jackets.attributes[0].options[0] === "true";
+    });
 
-  bestSellers(newJackets.reverse(), 6);
-}
-catch(error){
-  alert("There was an error retrieving the products. Pleases try reloading the page");
-  window.sessionStorage.clear();
+    bestSellers(newJackets.reverse(), 6);
+  }
+  catch(error){
+    alert("There was an error retrieving the products. Pleases try reloading the page");
+    window.sessionStorage.clear();
+  };
 };
 
 menu();
